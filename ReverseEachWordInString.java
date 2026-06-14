e275751474e7b046c3c1616ef9d49d63e323d4f3
Mohan-9969 <pandimohan168@gmail.com>
2026-06-14 16:25:40 +0530

    reverse each words in string is added

---
+++ ReverseEachWordInString.java
@@ -0,0 +1,23 @@
+public class ReverseEachWordInString {
+    public static void main(String[] args) {
+        String str="My name is mohan";
+        String [] words=str.split(" ");
+        for(int i=0;i<words.length;i++){
+            String s=words[i];
+            char [] arr=s.toCharArray();
+            int left=0;
+            int right=arr.length-1;
+            while(left<right){
+                char temp=arr[left];
+                arr[left]=arr[right];
+                arr[right]=temp;
+                left++;
+                right--;
+            }
+            words[i]=new String(arr);
+    }
+    System.out.println(String.join(" ",words));
+    }
+}
+    
+
